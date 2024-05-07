a0180d5b5a4c3728e4e2f82fdb8487fb6ce67e4c
grosrene <pybaudin@gmail.com>
2024-05-07 12:43:36 +0200

    various fixes on training docker

--- docker/training/train.sh
+++ docker/training/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nnUNetv2_plan_and_preprocess $1 --verify_dataset_integrity
+nnUNetv2_plan_and_preprocess -d $1 --verify_dataset_integrity
 
 nnUNetv2_train $1 $2 0
 nnUNetv2_train $1 $2 1
