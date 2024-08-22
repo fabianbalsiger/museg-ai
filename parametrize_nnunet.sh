041f9556c1f5f76e91f57349041dfd0790aac1e9
louis rigler <l.rigler@institut-myologie.org>
2024-08-22 14:55:13 +0200

    meaningless gitignore modification

---
+++ parametrize_nnunet.sh
@@ -0,0 +1,5 @@
+cd /mnt/rmn_files/0_Wip/New/1_Methodological_Developments/4_Methodologie_Traitement_Image/#8_2022_Re-Segmentation
+conda activate mutools-test
+export nnUNet_raw=./nnUNet_raw
+export nnUNet_preprocessed=./nnUNet_preprocessed
+export nnUNet_results=./nnUNet_results
